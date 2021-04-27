9ee483ee088fb57443f10dc21437efa9d7f43296
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-04-27 15:11:06 -0300

    add. sweetAlert

--- src/main.js
+++ src/main.js
@@ -7,10 +7,13 @@ import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import VueGoogleCharts from "vue-google-charts";
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
 
 Vue.use(VueGoogleCharts);
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(VueSweetalert2);
 
 Vue.config.productionTip = false;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,36 @@
 <template>
   <div class="home">
     <h1>Aqui estou na home page!</h1>
+    <b-button class="mr-2" variant="primary" @click="defaultAlert">SweetAlert2_default</b-button>
+    <b-button class="mr-2" variant="success" @click="successAlert">SweetAlert2_success</b-button>
+    <b-button class="mr-2" variant="danger" @click="errorAlert">SweetAlert2_success</b-button>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+
+  },
+
+  methods: {
+    defaultAlert(){
+      this.$swal("HelloWorld!", "🌍 🌎 🌏")
+    },
+
+    successAlert(){
+      this.$swal("Success!", "I am so proud with this success. 😀", "success")
+    },
+
+    errorAlert(){
+      this.$swal("Error!", "Oww no dude. 😭", "error")
+    },
+
+
+  }
+
 };
 </script>
