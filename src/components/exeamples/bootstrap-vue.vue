8da7179769e5804828b09ab8ad87a294dbfa0ac5
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-04-28 18:57:23 -0300

    update. modal example

--- src/components/exeamples/bootstrap-vue.vue
+++ src/components/exeamples/bootstrap-vue.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
     <b-card id="def" class="mb-2" title="Bootstrap Vue">
+      <!-- MODAL -->
+      <div class="col-md-12 mb-5">
+        <h5>Modal</h5>
+        <div>
+          <b-button @click="showMsgOk">Show OK message box with custom content</b-button>
+        </div>
+      </div>
+
       <!-- CARDS -->
       <div class="col-md-12 mb-5 mt-5">
       <h5>Cards</h5>
@@ -113,7 +121,36 @@ export default {
       //f3
       onContext_f3(ctx) {
         this.context_f3 = ctx
-      }
+      },
+      //modal
+      showMsgOk() {
+        const h = this.$createElement
+        // Using HTML string
+        const titleVNode = h('div', { domProps: { innerHTML: 'Title from <i>HTML<i> string' } })
+        // More complex structure
+        const messageVNode = h('div', { class: ['foobar'] }, [
+          h('p', { class: ['text-center'] }, [
+            ' Flashy ',
+            h('strong', 'msgBoxOk'),
+            ' message ',
+          ]),
+          h('p', { class: ['text-center'] }, [h('b-spinner')]),
+          h('b-img', {
+            props: {
+              src: 'https://picsum.photos/id/20/250/250',
+              thumbnail: true,
+              center: true,
+              fluid: true, rounded: 'circle'
+            }
+          })
+        ])
+        // We must pass the generated VNodes as arrays
+        this.$bvModal.msgBoxOk([messageVNode], {
+          title: [titleVNode],
+          buttonSize: 'sm',
+          centered: true, size: 'sm'
+        })
+      },
     }
     
 }
