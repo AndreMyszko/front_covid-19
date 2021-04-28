bfec0c082338916959088f1dc930edbf9f60393b
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-04-28 14:48:28 -0300

    add. styling frontend

--- src/components/exeamples/sweetAlert2.vue
+++ src/components/exeamples/sweetAlert2.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <b-card title="Sweet Alert 2">
+    <b-card id="def" class="mb-2" title="Sweet Alert 2">
 
       <b-card-text>
         <h5>Normal</h5>
@@ -106,3 +106,10 @@ export default {
 
 }
 </script>
+
+<style scoped>
+#def{
+  color: rgb(255, 255, 255);
+  background-image: linear-gradient(45deg, rgb(20, 222, 124) 0%, rgb(20, 222, 124) 21%,rgb(25, 190, 119) 21%, rgb(25, 190, 119) 37%,rgb(30, 158, 113) 37%, rgb(30, 158, 113) 46%,rgb(35, 126, 108) 46%, rgb(35, 126, 108) 53%,rgb(39, 94, 103) 53%, rgb(39, 94, 103) 59%,rgb(44, 62, 97) 59%, rgb(44, 62, 97) 77%,rgb(49, 30, 92) 77%, rgb(49, 30, 92) 100%);
+}
+</style>
