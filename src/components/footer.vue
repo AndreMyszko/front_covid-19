5e6e431d57342089c5d7cb3ebd659dac74004c2b
ANDRÉ MYSZKO <andre.myszko@gmail.com>
2021-04-28 23:33:15 -0300

    update. style

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,8 @@
 <template>
   <footer class="text-center text-lg-start">
     <div id="footer" class="text-center p-3">
-      <iframe src="https://giphy.com/embed/bxefaqWHgPD7G" width="120" height="115" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/stickers/bxefaqWHgPD7G"></a></p>
+      <iframe src="https://giphy.com/embed/iiEZmNmK1cZBWWgBKY" width="200" height="200" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+      <br>
       © 2021 Copyright:<a class="text-light" href="#"> COVID opendata</a>
     </div>
   </footer>
